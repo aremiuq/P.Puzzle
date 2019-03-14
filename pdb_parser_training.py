8afb13a294cf92e4f334cf93ee7cdf7bb8931489
aremiuq <aleix.matabacas@gmail.com>
2019-03-14 18:45:55 +0100

    Get sequence and Check directory functions implemented

--- pdb_parser_training.py
+++ pdb_parser_training.py
@@ -1,28 +1,68 @@
-import Bio.PDB
+import Bio.PDB as pdb
 
-pdb_parser = Bio.PDB.PDBParser(PERMISSIVE=True)
+import os
 
-pdb_file = pdb_parser.get_structure("pdb_file", "./example1/pair_his3_sc_XA.pdb")
+def Check_folder(folder):
+    """If folder don't exists creates it in the actual path"""
+    path = os.getcwd()
 
-class chain1(Bio.PDB.Select):
-    def accept_chain(self, chain):
-        if chain.get_id()=="A":
-            return True
-        else:
+    new_path = path + "/" + folder
+
+    if not os.path.isdir(new_path):
+
+        try:
+            os.mkdir(new_path)
+        except OSError:
+            print ("Creation of the directory %s failed" % path)
             return False
-
-class chain2(Bio.PDB.Select):
-    def accept_chain(self, chain):
-        if chain.get_id()=="B":
-            return True
         else:
-            return False
+            print ("Successfully created the directory %s " % path)
 
-io = Bio.PDB.PDBIO()
+    return True
 
-io.set_structure(pdb_file)
+def Get_Chains(pdb_file):
+    """Separate the two chains and return their name in a list"""
 
-io.save("chain1.pdb", chain1())
+    folder = "pdb_chains"
 
-io.save("chain2.pdb", chain2())
+    if not Check_folder(folder):
 
+        return False
+
+    pdb_parser = pdb.PDBParser(PERMISSIVE=True, QUIET=True)
+
+    pdb_structure = pdb_parser.get_structure("pdb_file", pdb_file)
+
+    interaction = list(pdb_file[:-4].split("_")[-1]) # Obtain 2 len list with the chain names from file name, the order of the letters need match with the order in the pdb file(format= something_chains.pdb)
+
+    if len(interaction) != 2: #if the length is not true, something goes wrong
+
+        return error
+
+    i=0
+
+    for model in pdb_structure:
+        for chain in model:
+
+            id=chain.get_id()
+
+            class chain(pdb.Select):
+                def accept_chain(self, chain):
+                    if chain.get_id()== id:
+                        return True
+                    else:
+                        return False
+
+            io = pdb.PDBIO()
+
+            io.set_structure(pdb_structure)
+
+            io.save("./%s/chain_%s.pdb" %(folder,interaction[i]), chain())
+
+            i += 1
+
+    return interaction
+
+A=Get_Chains("./example1/pair_his3_sc_XA.pdb")
+
+print(A)
